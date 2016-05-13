6400ed75227e157ad59036212a5fde28fd4cc764
Leo Liu <leo.liu@vml.com.au>
2016-05-13 12:22:31 +1000

    fixed an issue that causes the drawer closing when user swipes up while drawer is open

--- resources/FVVerticalSlideView.m
+++ resources/FVVerticalSlideView.m
@@ -160,7 +160,8 @@
         {
             if(swipeVelocity.y < -100.0f)
             {
-                [self slideToBottom];
+                if (translate.y >= 0 && prevTranslate.y >= 0)
+                    [self slideToBottom];
             }
             else if((center.y + translate.y + swipeVelocity.y * inertiaSeconds) > (translationView.frame.size.height+topY))
             {
